519998a3acdba8f33c9f8a016c417eafcee40f72
rajdeep200 <69335542+rajdeep200@users.noreply.github.com>
2021-05-22 09:29:09 +0530

    react router implementation

--- procommerce-frontend/src/App.js
+++ procommerce-frontend/src/App.js
@@ -1,20 +1,25 @@
-import React from "react"
-import './App.css';
-import { Container } from 'react-bootstrap'
-import Header from './components/header'
-import Footer from './components/footer'
+import React from "react";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/header";
+import Footer from "./components/footer";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <HomePage />
-        </Container>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/product/:id" component={ProductPage} />
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }
