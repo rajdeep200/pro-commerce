fc3d93f9d26e4a76c8afd2b95e9b28738bdda16b
rajdeep200 <69335542+rajdeep200@users.noreply.github.com>
2021-05-22 08:37:36 +0530

    Header and Footer finished

--- procommerce-frontend/src/App.js
+++ procommerce-frontend/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import { Container } from 'react-bootstrap'
+import Header from './components/header'
+import Footer from './components/footer'
 
 function App() {
   return (
-    <div className="App">
-     ECOMMERCE
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
