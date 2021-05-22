aed75319c98dd6be62805b08a8c1fe4622ff9f86
rajdeep200 <69335542+rajdeep200@users.noreply.github.com>
2021-05-22 22:57:12 +0530

    connected backend with frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const dotenv = require("dotenv")
 const products = require('./data/products')
 
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -16,4 +18,6 @@ app.get('/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`server is running on ${PORT}`))
--- procommerce-frontend/src/pages/ProductPage.jsx
+++ procommerce-frontend/src/pages/ProductPage.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import axios from "axios";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductPage = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+
+const [product, setProduct] = useState({})
+useEffect(() => {
+  const fetchProduct = async () => {
+    const {data} = await axios.get(`/products/${match.params.id}`)
+    setProduct(data)
+  }
+  fetchProduct()
+}, [match])
 
   return (
     <>
