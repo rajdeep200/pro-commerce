a189e0e6f540c2299cbd9e985af745f00f80af62
rajdeep200 <69335542+rajdeep200@users.noreply.github.com>
2021-05-23 17:10:56 +0530

    send sample data to databas

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex:true
     });
     console.log(`Database running on ${connection.connection.host}`)
   } catch (error) {
